c51b6d47c78ee94b1fa5b486f7edd542640a19de
Gabriel Silva <gabrielsilva2799@hotmail.com>
2024-09-17 16:41:33 -0300

    FEATURE: Header creation

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,13 @@
 const Home = () => {
   return (
-    <div>
-      <h1>React + TypeScript App</h1>
-      <p>Home</p>
+    <div className="mx-8 md:mx-auto max-w-[1440px]">
+      <div className="flex justify-between items-center gap-5 bg-blue-200 rounded-b-2xl px-8 py-4">
+        <h2>Logo</h2>
+        <div className="flex items-center gap-5">
+          <p>Teste@teste</p>
+          <button>Sair</button>
+        </div>
+      </div>
     </div>
   );
 };
